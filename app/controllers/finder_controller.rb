c7c7f6304e537970ad769f3c9bfded73c7442318
John <johnconley507@hotmail.com>
2016-11-21 16:20:30 -0500

    updating controller and views

--- app/controllers/finder_controller.rb
+++ app/controllers/finder_controller.rb
@@ -1,17 +1,19 @@
 class FinderController < ApplicationController
 
 	def index
-		@page.total = 'All Customers'
+		@page_title = 'All Customers'
 		@customers = Customer.all
 	end
 
 	def alphabetized
+		@page_total = 'Alphabetized Customers'
 		@customers = Customer.order(:full_name, :last_name)
-		render: index
+		render :index
 	end
 	
 	def missing_email
+		@page_total = 'Customers Missing Email Addresses'
 		@customers = Customer.where(email_address: '')
-		render: index
+		render :index
 	end
 end
