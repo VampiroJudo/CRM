9f7477586ac0812c7828119c85f8513b770e3a89
John <johnconley507@hotmail.com>
2016-11-25 15:42:03 -0500

    adding states and provinces in active admin

--- app/admin/customer.rb
+++ app/admin/customer.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Customer do
-	permit_params :full_name, :last_name, :phone_number, :email_address, :notes
+	permit_params :full_name, :last_name, :phone_number, :email_address, :notes, :province_id, :state_id
 
 
 
