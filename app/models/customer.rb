abd0f8ad5ef474326d7f2b84d46bd42d4c9bfa57
John <johnconley507@hotmail.com>
2016-11-25 15:29:40 -0500

    adding validations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,7 @@
 class Customer < ApplicationRecord
 	belongs_to :province
 	belongs_to :state
+
+	validates :full_name, presense: true
+	validates :last_name, presense: true
 end
--- app/models/province.rb
+++ app/models/province.rb
@@ -1,3 +1,8 @@
 class Province < ApplicationRecord
 	has_many :customers
+
+
+	
+	validates :full_name, presense: true
+	validates :last_name, presense: true
 end
